c4f4f2c4a6554d973ac44ebf3b85585a710e88cb
kizuner-bonely <1003306162@qq.com>
2024-10-28 00:57:27 +0800

    feat: init book module and remove useless service file

--- projects/2-library-system/be/src/app.module.ts
+++ projects/2-library-system/be/src/app.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common'
 
 //* app module
 import { AppController } from './app.controller'
-import { AppService } from './app.service'
 //* custom modules
-import { UserModule } from './user/user.module'
 import { DbModule } from './db/db.module'
+import { UserModule } from './user/user.module'
+import { BookModule } from './book/book.module'
 
 @Module({
-  imports: [UserModule, DbModule],
+  imports: [UserModule, DbModule, BookModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
