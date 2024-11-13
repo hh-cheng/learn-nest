d35cbbc827aab880e3a4cd200587a1b9f16ec3e7
kizuner-bonely <1003306162@qq.com>
2024-11-13 23:38:50 +0800

    feat: implement basic use of session

---
+++ projects/5-jwt-session/src/app.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Session } from '@nestjs/common'
+
+import { AppService } from './app.service'
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('test-session')
+  testSession(@Session() session) {
+    console.log(session)
+    session.count = session.count ? session.count + 1 : 1
+    return session.count
+  }
+}
---
+++ projects/5-jwt-session/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core'
+import * as session from 'express-session'
+
+import { AppModule } from './app.module'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+
+  app.use(
+    session({
+      secret: 'hh',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  )
+
+  await app.listen(process.env.PORT ?? 3000)
+}
+
+bootstrap()
