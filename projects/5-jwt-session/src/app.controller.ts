0a3e67e374cdefd2ad96988a755dfa87374b3962
kizuner-bonely <1003306162@qq.com>
2024-11-13 23:41:44 +0800

    chore: add session type

--- projects/5-jwt-session/src/app.controller.ts
+++ projects/5-jwt-session/src/app.controller.ts
@@ -7,7 +7,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('test-session')
-  testSession(@Session() session) {
+  testSession(@Session() session: Record<string, any>) {
     console.log(session)
     session.count = session.count ? session.count + 1 : 1
     return session.count
