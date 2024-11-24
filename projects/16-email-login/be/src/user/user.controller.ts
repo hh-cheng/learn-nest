f22d19d3b4ba91c16383f935b4893702249a4d6c
kizuner-bonely <1003306162@qq.com>
2024-11-24 17:03:39 +0800

    feat: implement login

--- projects/16-email-login/be/src/user/user.controller.ts
+++ projects/16-email-login/be/src/user/user.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common'
+import { omit } from 'es-toolkit'
+import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common'
+
 import { UserService } from './user.service'
+import { LoginUserDto } from './dto/user.dto'
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post('login')
+  async login(@Body() loginUser: LoginUserDto) {
+    const { email, code } = loginUser
+
+    const codeInRedis = await this.userService.findCodeByEmail(email)
+    if (code !== codeInRedis) {
+      throw new UnauthorizedException('verification code error')
+    }
+
+    const user = await this.userService.findUserByEmail(email)
+
+    return omit(user, ['password'])
+  }
 }
--- projects/16-email-login/be/src/user/user.service.ts
+++ projects/16-email-login/be/src/user/user.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common'
+import type { EntityManager } from 'typeorm'
+import { InjectEntityManager } from '@nestjs/typeorm'
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common'
+
+import { User } from './entities/user.entity'
+import { RedisService } from 'src/redis/redis.service'
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  @Inject()
+  private readonly redisService: RedisService
+
+  @InjectEntityManager()
+  private readonly entityManager: EntityManager
+
+  async findCodeByEmail(email: string) {
+    const codeInRedis = await this.redisService.get(`captcha_${email}`)
+    if (!codeInRedis) {
+      throw new UnauthorizedException('verification code expired')
+    }
+    return codeInRedis
+  }
+
+  findUserByEmail(email: string) {
+    return this.entityManager.findOneBy(User, { email })
+  }
+}
