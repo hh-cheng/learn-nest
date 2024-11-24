94be925ac1af3f9df4cb8d73afc1ca459ef6e530
kizuner-bonely <1003306162@qq.com>
2024-11-24 23:08:09 +0800

    feat: implement login

--- projects/17-article-views/src/user/user.controller.ts
+++ projects/17-article-views/src/user/user.controller.ts
@@ -1,8 +1,20 @@
-import { Controller } from '@nestjs/common'
+import { tap } from 'rxjs'
+import type { Session as SessionType } from 'express-session'
+import { Body, Controller, Post, Session } from '@nestjs/common'
 
 import { UserService } from './user.service'
+import { LoginUserDto } from './dto/login-user.dto'
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post('login')
+  login(@Body() loginUser: LoginUserDto, @Session() session: SessionType) {
+    return this.userService.login(loginUser).pipe(
+      tap((user) => {
+        session.user = { id: user.id, username: user.username }
+      }),
+    )
+  }
 }
--- projects/17-article-views/src/user/user.service.ts
+++ projects/17-article-views/src/user/user.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common'
+import { omit } from 'es-toolkit'
+import { from, map, tap } from 'rxjs'
+import type { EntityManager } from 'typeorm'
+import { InjectEntityManager } from '@nestjs/typeorm'
+import { BadRequestException, Injectable } from '@nestjs/common'
+
+import { User } from './entities/user.entity'
+import { LoginUserDto } from './dto/login-user.dto'
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  @InjectEntityManager()
+  private readonly entityManager: EntityManager
+
+  login(loginUser: LoginUserDto) {
+    return from(
+      this.entityManager.findOneBy(User, { username: loginUser.username }),
+    ).pipe(
+      tap((user) => {
+        if (!user) {
+          throw new BadRequestException('User not found')
+        }
+        if (user.password !== loginUser.password) {
+          throw new BadRequestException('Invalid password')
+        }
+      }),
+      map((user) => omit(user, ['password'])),
+    )
+  }
+}
