998051c1868362f9bfb44468d7c8bcd2102a15a3
kizuner-bonely <1003306162@qq.com>
2024-11-24 23:17:21 +0800

    feat: init article api

--- projects/17-article-views/src/article/article.controller.ts
+++ projects/17-article-views/src/article/article.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, Param } from '@nestjs/common'
 import { ArticleService } from './article.service'
 
 @Controller('article')
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.articleService.findOne(id)
+  }
+
+  @Get('/view/:id')
+  view(@Param('id') id: string) {
+    return this.articleService.view(id)
+  }
 }
--- projects/17-article-views/src/article/article.service.ts
+++ projects/17-article-views/src/article/article.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common'
+import type { EntityManager } from 'typeorm'
+import { InjectEntityManager } from '@nestjs/typeorm'
+import { Article } from './entities/article.entity'
 
 @Injectable()
-export class ArticleService {}
+export class ArticleService {
+  @InjectEntityManager()
+  private readonly entityManager: EntityManager
+
+  findOne(id: string) {
+    return this.entityManager.findOneBy(Article, { id: +id })
+  }
+
+  async view(id: string) {
+    const article = await this.findOne(id)
+    article.viewCount++
+    await this.entityManager.save(article)
+    return article.viewCount
+  }
+}
