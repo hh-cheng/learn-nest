64873532989b4a41fcb572a7443924bdc27ded35
kizuner-bonely <1003306162@qq.com>
2024-11-25 23:49:56 +0800

    feat: implement query weather of specific city

--- projects/19-city-weather/src/app.controller.ts
+++ projects/19-city-weather/src/app.controller.ts
@@ -1,14 +1,29 @@
+import { map } from 'rxjs'
 import pinyin from 'pinyin'
-import { Controller, Get, Query } from '@nestjs/common'
-
-import { AppService } from './app.service'
+import { HttpService } from '@nestjs/axios'
+import { ConfigService } from '@nestjs/config'
+import { Controller, Get, Inject, Param, Query } from '@nestjs/common'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  @Inject()
+  private readonly httpService: HttpService
+
+  @Inject()
+  private readonly configService: ConfigService
 
   @Get('pinyin')
   pinyin(@Query('text') text: string) {
     return pinyin(text, { style: 'normal' }).join('')
+  }
+
+  @Get('weather/:city')
+  weather(@Param('city') city: string) {
+    const cityPinyin = pinyin(city, { style: 'normal' }).join('')
+    return this.httpService
+      .get(
+        `https://geoapi.qweather.com/v2/city/lookup?location=${cityPinyin}&key=${this.configService.get('breeze')}`,
+      )
+      .pipe(map(({ data }) => data))
   }
 }
