b0aa2ab03994f159f856b3faa811c4e25fadc9f0
kizuner-bonely <1003306162@qq.com>
2024-11-26 23:50:34 +0800

    feat: add sse test api

--- projects/22-sse/src/app.controller.ts
+++ projects/22-sse/src/app.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Sse } from '@nestjs/common'
 import { AppService } from './app.service'
+import { Observable } from 'rxjs'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Sse('stream')
+  stream() {
+    return new Observable((observer) => {
+      observer.next({ data: { msg: 'hh' } })
+
+      setTimeout(() => {
+        observer.next({ data: { msg: 'smq' } })
+      }, 2000)
+
+      setTimeout(() => {
+        observer.next({ data: { msg: 'hh loves smq' } })
+      }, 5000)
+    })
+  }
 }
