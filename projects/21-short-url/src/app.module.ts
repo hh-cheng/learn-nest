297af46f28157b5502a8cc34781df810d456497a
kizuner-bonely <1003306162@qq.com>
2024-11-26 22:21:07 +0800

    feat: init 21-short-url

---
+++ projects/21-short-url/src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+
+import { AppService } from './app.service'
+import { AppController } from './app.controller'
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'hh',
+      database: 'short-url',
+      synchronize: true,
+      logging: true,
+      entities: [],
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      extra: { authPlugins: 'sha256_password' },
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
