1f2235dff45e677e9d1185ba4dcac073668fb218
kizuner-bonely <1003306162@qq.com>
2024-11-26 22:11:01 +0800

    feat: implement getting city by ip

--- projects/20-request-log/src/app.module.ts
+++ projects/20-request-log/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
+import { HttpModule } from '@nestjs/axios'
+import { APP_INTERCEPTOR } from '@nestjs/core'
+
 import { AppService } from './app.service'
+import { AppController } from './app.controller'
+import { RequestLogInterceptor } from './request-log.interceptor'
 
 @Module({
-  imports: [],
+  imports: [HttpModule.register({ timeout: 5000 })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: RequestLogInterceptor,
+    },
+  ],
 })
 export class AppModule {}
--- projects/20-request-log/src/request-log.interceptor.ts
+++ projects/20-request-log/src/request-log.interceptor.ts
@@ -1,26 +1,55 @@
 import {
   CallHandler,
   ExecutionContext,
+  Inject,
   Injectable,
   Logger,
   NestInterceptor,
 } from '@nestjs/common'
+import * as iconv from 'iconv-lite'
 import { Observable, tap } from 'rxjs'
+import * as requestIP from 'request-ip'
+import { HttpService } from '@nestjs/axios'
 import type { Request, Response } from 'express'
 
 @Injectable()
 export class RequestLogInterceptor implements NestInterceptor {
   private readonly logger = new Logger(RequestLogInterceptor.name)
 
-  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+  @Inject()
+  private readonly httpService: HttpService
+
+  async getCityByIp(ip: string) {
+    const response = await this.httpService.axiosRef(
+      `https://whois.pconline.com.cn/ipJson.jsp?ip=${ip}&json=true`,
+      {
+        responseType: 'arraybuffer',
+        transformResponse: [
+          (data) => {
+            const str = iconv.decode(data, 'gbk')
+            return JSON.parse(str)
+          },
+        ],
+      },
+    )
+    return response.data.addr
+  }
+
+  async intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Promise<Observable<any>> {
     const request = context.switchToHttp().getRequest<Request>()
     const response = context.switchToHttp().getResponse<Response>()
     const userAgent = request.headers['user-agent']
 
     const { ip, method, path } = request
+    const clientIP = await this.getCityByIp(
+      requestIP.getClientIp(request) || ip,
+    )
 
     this.logger.debug(
-      `${method} ${path} ${ip} ${userAgent}: ${context.getClass().name} ${
+      `${method} ${path} ${clientIP} ${userAgent}: ${context.getClass().name} ${
         context.getHandler().name
       } invoked...`,
     )
@@ -30,7 +59,7 @@ export class RequestLogInterceptor implements NestInterceptor {
     return next.handle().pipe(
       tap((res) => {
         this.logger.debug(
-          `${method} ${path} ${ip} ${userAgent}: ${response.statusCode}: ${Date.now() - now}ms`,
+          `${method} ${path} ${clientIP} ${userAgent}: ${response.statusCode}: ${Date.now() - now}ms`,
         )
         this.logger.debug(`Response: ${JSON.stringify(res)}`)
       }),
