d150a017c1e797a0da58b1b8f140b59b616f778e
kizuner-bonely <1003306162@qq.com>
2024-11-27 23:58:25 +0800

    feat: implement stream download

--- projects/25-large-file-download/src/app.controller.ts
+++ projects/25-large-file-download/src/app.controller.ts
@@ -1,7 +1,29 @@
-import { Controller } from '@nestjs/common'
-import { AppService } from './app.service'
+import * as fs from 'fs'
+import type { Response } from 'express'
+import { Controller, Get, Header, Res, StreamableFile } from '@nestjs/common'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  @Get('download')
+  download(@Res() res: Response) {
+    const pkg = fs.readFileSync('package.json')
+    res.set('Content-Disposition', 'attachment; filename="package.json"')
+    res.end(pkg)
+  }
+
+  @Get('download2')
+  @Header('content-disposition', 'attachment; filename="package.json"')
+  download2(@Res() res: Response) {
+    const stream = fs.createReadStream('package.json')
+    stream.pipe(res)
+  }
+
+  @Get('download3')
+  download3() {
+    const stream = fs.createReadStream('package.json')
+    return new StreamableFile(stream, {
+      type: 'text/plain',
+      disposition: 'attachment; filename="package.json"',
+    })
+  }
 }
