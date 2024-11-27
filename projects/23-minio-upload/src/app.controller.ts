0dd88afb533fcc09ae13aa890f9f9a37cced6fda
kizuner-bonely <1003306162@qq.com>
2024-11-27 21:10:17 +0800

    feat: implement test api for minio upload

--- projects/23-minio-upload/src/app.controller.ts
+++ projects/23-minio-upload/src/app.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common'
+import * as Minio from 'minio'
+import { Controller, Get, Inject } from '@nestjs/common'
+
 import { AppService } from './app.service'
+import { MINIO_CLIENT } from './minio/minio.module'
 
 @Controller()
 export class AppController {
+  @Inject(MINIO_CLIENT)
+  private readonly minioClient: Minio.Client
+
   constructor(private readonly appService: AppService) {}
+
+  @Get('test-upload')
+  async testUpload() {
+    try {
+      await this.minioClient.fPutObject('test', 'hello.txt', './package.json')
+      return 'http://localhost:9000/test/hello.txt'
+    } catch (err) {
+      console.log(err)
+      return 'Upload failed'
+    }
+  }
 }
