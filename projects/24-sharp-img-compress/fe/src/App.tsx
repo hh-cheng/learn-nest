d5aac89319156eb2810175b89ecf7ef49c721440
kizuner-bonely <1003306162@qq.com>
2024-11-27 22:02:46 +0800

    feat: init fe demo of img compress

--- projects/24-sharp-img-compress/fe/src/App.tsx
+++ projects/24-sharp-img-compress/fe/src/App.tsx
@@ -1,3 +1,26 @@
+import { message, Upload } from 'antd'
+import { InboxOutlined } from '@ant-design/icons'
+
+const { Dragger } = Upload
+
 export default function App() {
-  return <div>app</div>
+  return (
+    <Dragger
+      name="file"
+      action="http://localhost:3000"
+      onChange={(info) => {
+        const { status } = info.file
+        if (status === 'done') {
+          message.success(`${info.file.name} file uploaded successfully`)
+        } else {
+          message.error(`${info.file.name} file upload failed`)
+        }
+      }}
+    >
+      <p className="ant-upload-drag-icon">
+        <InboxOutlined />
+      </p>
+      <p className="ant-upload-text">click or drag a file here to upload</p>
+    </Dragger>
+  )
 }
