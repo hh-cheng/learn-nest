da20bc542aa779608058d9829fd7af125511c6d0
kizuner-bonely <1003306162@qq.com>
2024-11-28 23:09:09 +0800

    feat: implement basic i18n

--- projects/27-i18n/src/app.module.ts
+++ projects/27-i18n/src/app.module.ts
@@ -1,10 +1,32 @@
+import * as path from 'path'
 import { Module } from '@nestjs/common'
+import {
+  AcceptLanguageResolver,
+  CookieResolver,
+  HeaderResolver,
+  I18nModule,
+  QueryResolver,
+} from 'nestjs-i18n'
 
 import { AppService } from './app.service'
 import { AppController } from './app.controller'
 
 @Module({
-  imports: [],
+  imports: [
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: path.join(__dirname, '/i18n/'),
+        watch: true,
+      },
+      resolvers: [
+        new QueryResolver(['lang', 'l']),
+        new HeaderResolver(['x-custom-lang']),
+        new CookieResolver(['lang']),
+        AcceptLanguageResolver,
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- projects/27-i18n/src/app.service.ts
+++ projects/27-i18n/src/app.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common'
+import { I18nContext, I18nService } from 'nestjs-i18n'
+import { Inject, Injectable } from '@nestjs/common'
 
 @Injectable()
-export class AppService {}
+export class AppService {
+  @Inject()
+  private readonly i18n: I18nService
+
+  getHello() {
+    return this.i18n.t('test.hello', { lang: I18nContext.current().lang })
+  }
+}
