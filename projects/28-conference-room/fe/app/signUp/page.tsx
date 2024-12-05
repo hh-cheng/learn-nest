35c12e4ce021f103fdb2b0fb32232f5dcdfbc4ab
kizuner-bonely <1003306162@qq.com>
2024-12-05 23:18:33 +0800

    feat: add redirect in sign up page

--- projects/28-conference-room/fe/app/signUp/page.tsx
+++ projects/28-conference-room/fe/app/signUp/page.tsx
@@ -1,6 +1,12 @@
+import { redirect } from 'next/navigation'
+import { getServerSession } from 'next-auth'
+
 import Form from './form'
 
 export default async function SignUpPage() {
+  const session = await getServerSession()
+  if (session) redirect('/')
+
   return (
     <div className="max-w-md m-auto mt-8">
       <Form />
