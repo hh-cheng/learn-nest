9897a5a366bf76f0dd55939c11c5979bfe2dc8ea
kizuner-bonely <1003306162@qq.com>
2024-12-07 20:13:39 +0800

    feat: setup layout of user dashboard

--- projects/28-conference-room/fe/app/(user)/layout.tsx
+++ projects/28-conference-room/fe/app/(user)/layout.tsx
@@ -1,4 +1,12 @@
 export default function UserLayout(props: { children: React.ReactNode }) {
   const { children } = props
-  return <div>{children}</div>
+
+  return (
+    <main>
+      <h1 className="border-b-2 h-14 flex items-center px-6 text-2xl">
+        Conference Room Management
+      </h1>
+      {children}
+    </main>
+  )
 }
